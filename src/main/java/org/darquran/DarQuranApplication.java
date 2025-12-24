3ce549cd37d68e53d51829daec6585b45064d03a
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-12-24 09:59:34 +0100

    first commit

---
+++ src/main/java/org/darquran/DarQuranApplication.java
@@ -0,0 +1,13 @@
+package org.darquran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarQuranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarQuranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/darquran/DarQuranApplicationTests.java
@@ -0,0 +1,13 @@
+package org.darquran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarQuranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
