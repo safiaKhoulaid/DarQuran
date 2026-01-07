61cd94097ac556f57b43bb1e6ca5a4aa00b18e60
safia-khoulaid <safiakhoulaid11@gmail.com>
2026-01-07 10:25:14 +0000

    Implement Admin class extending User with JPA annotations

--- src/main/java/com/darquran/domain/model/entities/users/Admin.java
+++ src/main/java/com/darquran/domain/model/entities/users/Admin.java
@@ -1,4 +1,18 @@
 package com.darquran.domain.model.entities.users;
 
-public class Admin {
+import com.darquran.domain.model.entities.users.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "admins")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+public class Admin extends User {
 }
