46983cb922234d0999a7f19e91b75b96aaf9885c
safia-khoulaid <safiakhoulaid11@gmail.com>
2026-03-18 11:11:56 +0000

    FEAT(QUR-66): Add UserRepository interface for user data access with email and telephone lookup

---
+++ src/main/java/com/darquran/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.darquran.domain.repository;
+
+import com.darquran.domain.model.entities.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String username);
+
+    Optional<User> findByTelephone(String telephone);
+}
